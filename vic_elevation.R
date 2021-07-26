37949a6abb2e50bf8f650ce1f0a41cf410c6f1dd
LachlanD <lachlan.d@gmail.com>
2021-07-27 03:05:55 +1000

    scaling factor corrected

--- vic_elevation.R
+++ vic_elevation.R
@@ -6,17 +6,29 @@ library(rayshader)
 ras <- raster::raster("vmelev_dem10m_Geotiff_GDA94_Vicgrid.tif")
 plot(ras)
 res(ras)
-low_res <- aggregate(ras, fact=10)
+
+scaling_factor <- 25
+low_res <- aggregate(ras, fact=scaling_factor)
 res(low_res)
 
 plot(low_res)
 elmat <- raster_to_matrix(low_res)
+
+#correct for scaling factor
+elmat<-elmat/scaling_factor
+
 
 elmat %>%
   sphere_shade(texture = "imhof1") %>%
   add_shadow(ray_shade(elmat), 0.5) %>%
   add_shadow(ambient_shade(elmat), 0) %>%
   plot_map()
+
+elmat %>%
+  sphere_shade(texture = "imhof1") %>%
+  add_shadow(ray_shade(elmat), 0.5) %>%
+  add_shadow(ambient_shade(elmat), 0) %>%
+  plot_3d(elmat, zscale = 10, fov = 0, theta = 135, zoom = 0.75, phi = 45, windowsize = c(1000, 800))
 
 
 #Wilsons prom crop
